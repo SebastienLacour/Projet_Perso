c6434b6a4c0049054f3d47335788d076e327d9e4
Sebastien Lacour <sebastien.lacour1994@gmail.com>
2023-03-08 18:11:11 +0100

    modification de la route signup qui créer maintenant un nouvel utilisateur tout en cryptant le mot de passe

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,21 +1,34 @@
 //On importe le schéma de donnée Mongoose des utilisateurs
 const userModel = require('../models/user')
 
+//On importe bcrypt, package qui va nous permettre de hasher le mot de passe
+const bcrypt = require('bcrypt')
+
 //Fonction qui gère la création d'un nouveau compte
 exports.signup = (req, res) => {
     console.log("           SIGNUP          ")
     console.log(req.body)
 
-    //On créer un nouvel utilisateur à partir du modèle de donnée utilisateur
-    const user = new userModel({
-        pseudo: req.body.pseudo,
-        email: req.body.email,
-        password: req.body.password
-    })
+    //On utilise la méthode hash de bcrypt pour hasher le mot de passe, 
+    //on fait 10 tours de sels, c'est à dire on crypte 10 fois le mot de passe
+    bcrypt.hash(req.body.password, 10)
+        .then((hashedpassword) => {
 
-    console.log(user)
+            //On créer un nouvel utilisateur à partir du modèle de donnée utilisateur tout en cryptant le mot de passe
+            const user = new userModel({
+                pseudo: req.body.pseudo,
+                email: req.body.email,
+                password: hashedpassword
+            })
 
-    user.save()
-    .then(() => res.status(201).json({message: req.body}))
-    .catch( error => res.status(500).json({ error }))
+            console.log(user)
+
+            //On enregistre le nouvel utilisateur dans la base de donnée
+            user.save()
+            .then(() => res.status(201).json({ message: user }))
+            .catch(error => res.status(400).json({ error }))
+        }
+        )
+        .catch( error => res.status(500).json({ error }))
+
 }
